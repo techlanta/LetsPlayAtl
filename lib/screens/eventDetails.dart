e8cbdfdf1d333c8e04f6bb6f2f7701a250da1298
Mohammed Saqib <saqibm128@gmail.com>
2019-09-16 11:02:39 -0400

    fixed small typos

--- lib/screens/eventDetails.dart
+++ lib/screens/eventDetails.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 
 class EventDetails extends StatefulWidget {
@@ -63,70 +62,4 @@ class _EventDetailsState extends State<EventDetails> {
       ),
     );
   }
-=======
-import 'package:flutter/material.dart';
-
-class EventDetails extends StatefulWidget {
-  @override
-  _EventDetailsState createState() => _EventDetailsState();
-
-}
-
-class _EventDetailsState extends State<EventDetails> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        title: Text("Event Details"),
-      ),
-      backgroundColor: Colors.green[50],
-      resizeToAvoidBottomPadding: false,
-//            body: Container(
-////                crossAxisAlignment: CrossAxisAlignment.center, children: <
-//                Widget>[
-      body:
-      Container(
-        child: ListView(
-            padding: const EdgeInsets.all(8.0),
-            children: <Widget>[
-              GestureDetector(
-                onTap: () {},
-                child: Container(
-                  height: 50,
-                  color: Colors.blue[600],
-                  child: const Center(child: Text('Name:')),
-                ),),
-              GestureDetector(
-                onTap: () {},
-                child: Container(
-                  height: 50,
-                  color: Colors.blue[600],
-                  child: const Center(child: Text('Date:')),
-                ),),
-              GestureDetector(
-                onTap: () {},
-                child: Container(
-                  height: 50,
-                  color: Colors.blue[600],
-                  child: const Center(child: Text('Start Time:')),
-                ),),
-              GestureDetector(
-                onTap: () {},
-                child: Container(
-                  height: 50,
-                  color: Colors.blue[600],
-                  child: const Center(child: Text('End Time:')),
-                ),),
-              GestureDetector(
-                onTap: () {},
-                child: Container(
-                  height: 50,
-                  color: Colors.blue[600],
-                  child: const Center(child: Text('Details:')),
-                ),),
-            ]),
-      ),
-    );
-  }
->>>>>>> 72be1c2c7f5fb47566b9102802d70be959f6ca61
 }
